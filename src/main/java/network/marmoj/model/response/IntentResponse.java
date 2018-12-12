7fafe077f8e342e881b316727d40e5a6d5f684cb
Joaquin Pablo Gonzalez <jpgonzalezra@gmail.com>
2018-12-12 12:27:08 -0300

    [Add]  IntentResponse status code

--- src/main/java/network/marmoj/model/response/IntentResponse.java
+++ src/main/java/network/marmoj/model/response/IntentResponse.java
@@ -1,4 +1,20 @@
 package network.marmoj.model.response;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
+
 public class IntentResponse {
+
+    private HttpResponseStatus status;
+
+    public IntentResponse(HttpResponseStatus status) {
+        this.status = status;
+    }
+
+    public HttpResponseStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpResponseStatus status) {
+        this.status = status;
+    }
 }
