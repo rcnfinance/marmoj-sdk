80666479b065ac1fe085add14bfb08701a5ee6e0
Joaquin Pablo Gonzalez <jpgonzalezra@gmail.com>
2018-12-12 12:30:24 -0300

    Example

--- src/main/java/network/marmoj/Application.java
+++ src/main/java/network/marmoj/Application.java
@@ -2,6 +2,8 @@ package network.marmoj;
 
 import network.marmoj.model.core.Intent;
 import network.marmoj.model.core.SignedIntent;
+import network.marmoj.model.data.ERC20Impl;
+import network.marmoj.service.IntentService;
 import network.marmoj.service.MarmoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,16 +12,11 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.web3j.abi.FunctionEncoder;
-import org.web3j.abi.TypeReference;
 import org.web3j.abi.datatypes.Address;
-import org.web3j.abi.datatypes.Bool;
-import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.generated.Uint256;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -27,7 +24,9 @@ public class Application implements CommandLineRunner {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     @Autowired
-    private MarmoService service;
+    private MarmoService marmoService;
+    @Autowired
+    private IntentService intentService;
 
     public static void main(String[] args) throws Exception {
 
@@ -40,29 +39,26 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        service.setup("https://ropsten.node.rcn.loans:8545", "0xe038d8fdfab5c8a0c9178dbe9e730b3ab1100c52", "512850c7ebe3e1ade1d0f28ef6eebdd3ba4e78748e0682f8fda6fc2c2c5b334a");
+        marmoService.setup("https://ropsten.node.rcn.loans:8545", "0xe038d8fdfab5c8a0c9178dbe9e730b3ab1100c52", "512850c7ebe3e1ade1d0f28ef6eebdd3ba4e78748e0682f8fda6fc2c2c5b334a");
 
+        ERC20Impl erc20 = new ERC20Impl();
+        String transferDataCall = erc20.transfer(
+            new Address("0x7F5EB5bB5cF88cfcEe9613368636f458800e62CB"),
+            new Uint256(1)
+        );
 
-        final Function transfer = new Function("transfer",
-                Arrays.asList(
-                        new Address("0x7F5EB5bB5cF88cfcEe9613368636f458800e62CB"),
-                        new Address("0x6684C2F982758685780b8d488c32fAfA4d008A53"),
-                        new Uint256(1)),
-                Arrays.asList(new TypeReference<Bool>() {}));
-
-        Intent intent = service.create(
+        Intent intent = marmoService.create(
                 new ArrayList<>(),
                 "0x7F5EB5bB5cF88cfcEe9613368636f458800e62CB",
                 BigInteger.ONE,
-                FunctionEncoder.encode(transfer),
+                transferDataCall,
                 BigInteger.ZERO,
                 BigInteger.valueOf(999999999),
                 null
                 );
 
-        SignedIntent signedIntent = service.sign(intent);
-
-
+        SignedIntent signedIntent = marmoService.sign(intent);
+        intentService.send(signedIntent);
 
         LOGGER.info("Result: " + signedIntent.toString());
     }
